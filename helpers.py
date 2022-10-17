31f20136220974ce31d8b1ce4cff3297c3f7ee4f
Katrin <katrinmisel@gmail.com>
2022-10-17 20:48:48 +0200

    keras started

--- helpers.py
+++ helpers.py
@@ -1,6 +1,11 @@
 import nltk
 import emoji
-
+from sklearn.metrics import classification_report, confusion_matrix
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
 
 stop_words = set(nltk.corpus.stopwords.words('english'))
 english_words = set(nltk.corpus.words.words())
@@ -22,6 +27,71 @@ def text_cleaner(text):
 
     text = [lem.lemmatize(w) for w in text] # lemmatize
 
-    text = [w for w in text if len(w) > 3] # keep only words longer than 3 characters
+    text = [w for w in text if len(w) > 2] # keep only words longer than 2 characters
 
     return text
+
+
+def model_Evaluate(model, X_test, y_test):
+    
+    # Predict values for Test dataset
+    y_pred = model.predict(X_test)
+
+    # Print the evaluation metrics for the dataset.
+    print(classification_report(y_test, y_pred))
+    
+    # Compute and plot the Confusion matrix
+    cf_matrix = confusion_matrix(y_test, y_pred)
+
+    categories  = ['Negative','Positive']
+    group_names = ['True Neg','False Pos', 'False Neg','True Pos']
+    group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+
+    labels = [f'{v1}\n{v2}' for v1, v2 in zip(group_names,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+
+    sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',
+                xticklabels = categories, yticklabels = categories)
+
+    plt.xlabel("Predicted values", fontdict = {'size':14}, labelpad = 10)
+    plt.ylabel("Actual values"   , fontdict = {'size':14}, labelpad = 10)
+    plt.title ("Confusion Matrix", fontdict = {'size':18}, pad = 20)
+
+
+
+### load the logistic regression model ###
+
+def load_lr_model():
+
+    # Load the vectoriser.
+    file = open('tfidf_vectoriser.pickle', 'rb')
+    vectoriser = pickle.load(file)
+    file.close()
+    # Load the LR Model.
+    file = open('Sentiment-LR.pickle', 'rb')
+    classification_model = pickle.load(file)
+    file.close()
+    
+    return vectoriser, classification_model
+
+
+
+### predict an array of strings with logistic regression ###
+
+def predict(text):
+
+    tfidfconverter, classification_model = load_lr_model()
+
+    # Predict the sentiment
+    textdata = tfidfconverter.transform(text_cleaner(text))
+    sentiment = classification_model.predict(textdata)
+    
+    # Make a list of text with sentiment.
+    data = []
+    for text, pred in zip(text, sentiment):
+        data.append((text,pred))
+
+    # Convert the list into a Pandas DataFrame.
+    df = pd.DataFrame(data, columns = ['text','sentiment'])
+    df = df.replace([0,4], ["Negative","Positive"])
+    return df
